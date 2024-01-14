37bb5dfcb826fde86d7bb77d37e66467d135dd72
nazmulkst <nazmulkst@gmail.com>
2024-01-15 00:42:33 +0600

    Mofidy PORT

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 // const express = require('express');
-const PORT = 5050;
+const PORT = process.env.PORT;
 
 app.listen(PORT, function(req, res){
     console.log('Application Run Success with Port: ' + PORT)
